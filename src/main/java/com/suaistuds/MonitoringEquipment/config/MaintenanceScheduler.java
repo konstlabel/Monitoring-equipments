d4b71e919531aeb22155a21b05b2bb5f9dc7b484
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 21:07:28 +0300

    Update MaintenanceScheduler.java

--- src/main/java/com/suaistuds/MonitoringEquipment/config/MaintenanceScheduler.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/config/MaintenanceScheduler.java
@@ -1,4 +1,12 @@
 package com.suaistuds.MonitoringEquipment.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+@EnableScheduling
 public class MaintenanceScheduler {
+    //todo
 }
