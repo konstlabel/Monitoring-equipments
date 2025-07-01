6d3dd0344f5ad5bae3d5bdd42a103593627fded5
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 20:44:39 +0300

    Update MaintenanceTask.java

--- src/main/java/com/suaistuds/MonitoringEquipment/model/entity/MaintenanceTask.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/model/entity/MaintenanceTask.java
@@ -1,4 +1,22 @@
-package model.entity;
+package com.suaistuds.MonitoringEquipment.model.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class MaintenanceTask {
+    @NonNull
+    private Long id;
+    @NonNull
+    private Equipment equipment;
+    @NonNull
+    private LocalDate scheduledDate;
+    @NonNull
+    private LocalDate completedDate;
+    @NonNull
+    private Status status;
 }
