6b4351f18ff5c9fdb6331d6ae526cf2ba917e6d3
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 20:44:26 +0300

    Update Equipment.java

--- src/main/java/com/suaistuds/MonitoringEquipment/model/entity/Equipment.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/model/entity/Equipment.java
@@ -1,4 +1,19 @@
-package model.entity;
+package com.suaistuds.MonitoringEquipment.model.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@Builder
 public class Equipment {
+    @NonNull
+    private Long id;
+    @NonNull
+    private TypeEquipment type;
+    @NonNull
+    private Status status;
+
+    private Long qrCodeValue;
+
 }
