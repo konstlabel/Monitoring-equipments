aed0bbe0e3accefb4db482c7b7d23b302c7a428b
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 20:45:22 +0300

    Update StatusHistory.java

--- src/main/java/com/suaistuds/MonitoringEquipment/model/entity/StatusHistory.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/model/entity/StatusHistory.java
@@ -1,4 +1,22 @@
-package model.entity;
+package com.suaistuds.MonitoringEquipment.model.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class StatusHistory {
+    @NonNull
+    private Long id;
+    @NonNull
+    private Equipment equipment;
+    @NonNull
+    private Status oldStatus;
+    @NonNull
+    private LocalDate timestamp;
+    @NonNull
+    private User changedByUser;
 }
