86532c38edf22db6607225b5f2342828d1110531
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 20:45:49 +0300

    Update TypeEquipment.java

--- src/main/java/com/suaistuds/MonitoringEquipment/model/entity/TypeEquipment.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/model/entity/TypeEquipment.java
@@ -1,5 +1,24 @@
 package com.suaistuds.MonitoringEquipment.model.entity;
 
-public enum TypeEquipment {
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.List;
+
+@Data
+@Builder
+public class User {
+    @NonNull
+    private final Long id;
+    @NonNull
+    private String login;
+    @NonNull
+    private String password;
+    @NonNull
+    private List<Role> roles;
+    @NonNull
+    private String email;
+
 
 }
