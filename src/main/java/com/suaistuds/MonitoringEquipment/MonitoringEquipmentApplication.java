b8f64a5ae97fa0e3b83aca345105bbcc5a0a4050
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 21:11:30 +0300

    Update MonitoringEquipmentApplication.java

--- src/main/java/com/suaistuds/MonitoringEquipment/MonitoringEquipmentApplication.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/MonitoringEquipmentApplication.java
@@ -2,12 +2,11 @@ package com.suaistuds.MonitoringEquipment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class MonitoringEquipmentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringEquipmentApplication.class, args);
 	}
-
 }
