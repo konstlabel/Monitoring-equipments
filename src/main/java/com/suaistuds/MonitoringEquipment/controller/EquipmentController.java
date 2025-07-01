3d8594cb63f44fa0a6492a5a6e8f36f4ee0dd4b9
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 21:08:16 +0300

    Update EquipmentController.java

--- src/main/java/com/suaistuds/MonitoringEquipment/controller/EquipmentController.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/controller/EquipmentController.java
@@ -1,4 +1,8 @@
-package controller;
+package com.suaistuds.MonitoringEquipment.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EquipmentController {
+    //todo
 }
