bbcc97c2556bef21008b9a5a74cd8bc17928d4ea
konstlabel <kostiauvarov123@gmail.com>
2025-07-01 22:38:39 +0300

    Initial commit

---
+++ src/main/java/com/suaistuds/monitoringequipments/MonitoringEquipmentsApplication.java
@@ -0,0 +1,11 @@
+package com.suaistuds.monitoringequipments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringEquipmentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringEquipmentsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/suaistuds/monitoringequipments/model/dto/ReservationDto.java
@@ -0,0 +1,4 @@
+package com.suaistuds.monitoringequipments.model.dto;
+
+public class ReservationDto {
+}
