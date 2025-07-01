bc5412887c0038ae3e4cfa6bb79b7de088462062
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 21:07:49 +0300

    Update WebConfig.java

--- src/main/java/com/suaistuds/MonitoringEquipment/config/WebConfig.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/config/WebConfig.java
@@ -1,4 +1,10 @@
 package com.suaistuds.MonitoringEquipment.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
 public class WebConfig {
+    //todo
 }
