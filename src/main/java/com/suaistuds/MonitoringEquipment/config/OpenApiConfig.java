f3ca244b2adcddaa0d43f3b9f403c1cda9942406
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 21:07:38 +0300

    Update OpenApiConfig.java

--- src/main/java/com/suaistuds/MonitoringEquipment/config/OpenApiConfig.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/config/OpenApiConfig.java
@@ -1,4 +1,11 @@
 package com.suaistuds.MonitoringEquipment.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Configuration
+@Component
 public class OpenApiConfig {
+    //todo
 }
