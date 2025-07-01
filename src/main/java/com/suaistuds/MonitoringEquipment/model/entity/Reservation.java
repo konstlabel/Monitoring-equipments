22cd091f804d85826f9c94b0e18debe4da86d973
Konstl <73719019+konstlabel@users.noreply.github.com>
2025-07-01 20:44:51 +0300

    Update Reservation.java

--- src/main/java/com/suaistuds/MonitoringEquipment/model/entity/Reservation.java
+++ src/main/java/com/suaistuds/MonitoringEquipment/model/entity/Reservation.java
@@ -1,4 +1,24 @@
-package model.entity;
+package com.suaistuds.MonitoringEquipment.model.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class Reservation {
+    @NonNull
+    private Long id;
+    @NonNull
+    private Equipment equipment;
+    @NonNull
+    private User user;
+    @NonNull
+    private LocalDateTime startTime;
+    @NonNull
+    private LocalDateTime endTime;
+    @NonNull
+    private Status status;
 }
